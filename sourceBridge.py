0f1a69dfb8f75a74f3b1b26625d05e5246bee395
IaVashik <matiyukv41@gmail.com>
2024-01-16 11:49:50 +0300

    code refactoring

--- sourceBridge.py
+++ sourceBridge.py
@@ -1,61 +1,68 @@
 import telnetlib
-import os
 import subprocess
-import time
 import psutil
 
-class SourceBridge():
 
-    tn = gameExe = gameDir = gameName = modDir = None
-    isConnected = False
+class SourceBridge:
+    def __init__(self):
+        self.tn = None
+        self.is_connected = False
+        self.connect()
+
 
     def connect(self):
-        if self.tn == None:
-            print("Trying to connect to game...")
-            try:
-                self.tn = telnetlib.Telnet("127.0.0.1", "2121")
-                self.isConnected = True
-                print("Connected to compatible Source game! (NetCon)")
-                return True
-            except:
-                self.tn = False
-                try:
-                    gameExecs = ["hl2.exe", "csgo.exe", "portal2.exe"]
-                    modDirs = {"GarrysMod": "garrysmod", "Team Fortress 2": "tf2", "Portal 2": "portal2", "Portal": "portal", "Counter-Strike Source": "cstrike", "Counter-Strike Global Offensive": "csgo", "Half-Life 2": "hl2"}
-                    for p in psutil.process_iter():
-                        if p.name() in gameExecs:
-                            self.gameExe = p.exe()
-                            self.gameDir = os.path.dirname(self.gameExe)
-                            self.gameName = os.path.basename(self.gameDir)
-                            self.modDir = os.path.join(self.gameDir, modDirs[self.gameName])
-                            self.isConnected = True
-                            print("Connected to "+self.gameName+"! (Hijack)")
-                            return True
-                except:
-                    pass
-                if not self.isConnected:
-                    print("Could not connect to compatible Source Game :(")
-                    return False
-        else:
-            return self.isConnected
+        if not self.connect_netcon():
+            self.connect_hijack()
+
+
+    def connect_netcon(self) -> bool:
+        try:
+            self.tn = telnetlib.Telnet("127.0.0.1", 2121)
+            self.is_connected = True
+            print("Connected via NetCon")
+            return True
+        except Exception:
+            return False
+
+
+    def connect_hijack(self) -> bool:
+        game = self.__find_game_process()
+        if game:
+            print(f"Connected to {game.name} via hijack")
+            self.is_connected = True
+            return True
+        return False
+
+    
+    def __find_game_process(self):
+        for process in psutil.process_iter():
+            if process.name() in ["hl2.exe", "csgo.exe", "portal2.exe"]:
+                return process
+        return None
+
+
+    def send_netcon_command(self, command):
+        if self.tn:
+            self.tn.write(f"{command}\n".encode("utf-8"))
+
+
+    def send_hijack_command(self, command):
+        if self.IsValid():
+            game = self.__find_game_process()
+            params = [game.exe(), "-hijack", "+", command]
+            subprocess.Popen(params, creationflags=subprocess.DETACHED_PROCESS) 
+
 
     def run(self, command):
-        if self.tn == None:
+        if self.IsValid() is False:
+            print("Source game offline now, try reconnect...")
             self.connect()
-        if self.isConnected:
-            if self.tn:
-                self.tn.write((str(command) + "\n").encode('utf-8'))
-            else:
-                launch_params = [self.gameExe, '-hijack', '+', command]
-                process = subprocess.Popen(
-                    launch_params,
-                    creationflags=subprocess.DETACHED_PROCESS |
-                    subprocess.CREATE_NEW_PROCESS_GROUP)
-                time.sleep(1)
             
-            return True
+        if self.tn:
+            self.send_netcon_command(command)
         else:
-            raise Exception("Compatible Source Game not Connected.")
-        
-    def __init__(self) -> None:
-        self.connect()
+            self.send_hijack_command(command)   # todo
+       
+            
+    def IsValid(self):
+        return self.is_connected # TODO
